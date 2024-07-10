3b7b30607b230a22a350fd25962a8bc864f338fc
NikolaiProgramist <nikluk2005@mail.ru>
2024-07-10 22:25:41 +0300

    Update src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,10 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting(): void
+function greeting(): string
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May i have your name?');
     line('Hello, %s!', $name);
+    return $name;
 }
