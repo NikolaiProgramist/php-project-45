a36f3bf9888f71d83314513ec20d1106fbf5f567
NikolaiProgramist <nikluk2005@mail.ru>
2024-07-11 18:38:17 +0300

    Remove unnecessary logic from src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,14 +1,3 @@
 <?php
 
 namespace BrainGames\Cli;
-
-use function cli\line;
-use function cli\prompt;
-
-function greeting(): string
-{
-    line('Welcome to the Brain Games!');
-    $name = prompt('May i have your name?');
-    line('Hello, %s!', $name);
-    return $name;
-}
