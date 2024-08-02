d0b69fb43654c646ca4a03f018e49cbdd602b163
NikolaiProgramist <nikluk2005@mail.ru>
2024-08-02 19:35:56 +0300

    Update logic of game 'brain-calc'

--- src/Games/BrainCalc.php
+++ src/Games/BrainCalc.php
@@ -3,7 +3,6 @@
 namespace BrainGames\Games\Brain\Calc;
 
 use function cli\line;
-use function BrainGames\Engine\questionAsk;
 use function BrainGames\Engine\preparationGame;
 
 const RULES = 'What is the result of the expression?';
@@ -11,20 +10,18 @@ const ROUND_COUNT = 3;
 
 function startBrainCalc(): void
 {
-    $name = preparationGame(RULES);
+    $data = [];
 
     for ($i = 1; $i <= ROUND_COUNT; $i++) {
         $operand1 = rand(0, 100);
         $operand2 = rand(0, 100);
         $operation = randomOperation();
 
-        $question = "Question: {$operand1} {$operation} {$operand2}";
-        $correctAnswer = calc($operand1, $operand2, $operation);
-
-        questionAsk($question, $correctAnswer, $name);
+        $data['questions'][] = "Question: {$operand1} {$operation} {$operand2}";
+        $data['correctAnswers'][] = calc($operand1, $operand2, $operation);
     }
 
-    line('Congratulations, %s!', $name);
+    preparationGame(RULES, ROUND_COUNT, $data);
 }
 
 function randomOperation(): string
